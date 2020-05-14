9f3970f232933abd02197a120574895391d6435d
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-05-14 11:24:17 -0300

    Add docker compose to project

---
+++ fabfile.py
@@ -0,0 +1,66 @@
+import sys
+
+from fabric import task
+from patchwork import info, packages
+
+
+@task
+def configure(ctx):
+    """
+    Configure host with Docker, docker-compose, etc.
+    """
+    print("Warning! This command is still under development!")
+
+    # Check distro
+    distro = info.distro_family(ctx)
+    if distro != "debian":
+        print(f"Only supports Debian-based distros, got '{distro}'", file=sys.stderr)
+        return
+
+    distro = info.distro_name(ctx)
+    print(f"Installing packages on {distro} machine...")
+    print("This recipe was tested on Ubuntu.")
+
+    # Install required packages
+    packages.package(ctx, "python3-pip")
+    ctx.run("pip3 install docker-compose")
+    git_clone(ctx)
+
+
+@task
+def git_clone(ctx):
+    """
+    Clone repository.
+    """
+    ctx.run("git clone https://github.com/pydemic/pydemic-ui.git app")
+
+
+@task
+def git_pull(ctx):
+    """
+    Update repository.
+    """
+    ctx.run("cd app && git pull -f")
+
+
+@task
+def update(ctx):
+    """
+    Update server
+    """
+    git_pull(ctx)
+    ctx.sudo("cd app && docker-compose build")
+    ctx.sudo("cd app && docker-compose down")
+    ctx.sudo("cd app && docker-compose up")
+
+
+@task
+def run(ctx, cmd):
+    """
+    Execute arbitrary command on remote server
+    """
+    res = ctx.run(cmd, pty=True)
+    if res.stdout:
+        print(res.stdout)
+    if res.stderr:
+        print(res.stderr, file=sys.stderr)
