64d307c57b209b4ead887706efdb26b96314a1f0
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-05-15 11:23:30 -0300

    Refactored apps to new API

--- pydemic_ui/apps/calc.py
+++ pydemic_ui/apps/calc.py
@@ -9,23 +9,27 @@ in the near future.
 
 This app uses components from the Pydemic UI package.
 """
+__package__ = "pydemic_ui.apps"
+
 import os
 
-import streamlit as st
+from pydemic_ui import st
 
 from pydemic.diseases import covid19
 from pydemic.models import SEAIR
 from pydemic.utils import extract_keys
 from pydemic_ui import info
 from pydemic_ui import ui
-from pydemic_ui.i18n import _
-from pydemic_ui.ui import (summary_cards, hospitalizations_chart, pause,
-                           available_beds_chart, population_info_chart, line,
-                           healthcare_parameters, epidemiological_parameters, footnotes,
-                           deaths_chart)
-from pydemic_ui.ui.output_components import protection_equipment_demand
+from pydemic_ui.ui import healthcare_parameters, epidemiological_parameters
+from ..i18n import _
+from ..ui import (
+    summary_cards,
+    population_info_chart,
+    deaths_chart,
+    protection_equipment_demand,
+)
 
-DEBUG = os.environ.get('DEBUG', 'false').lower() in ('true', 'on', '1')
+DEBUG = os.environ.get("DEBUG", "false").lower() in ("true", "on", "1")
 
 DEATH_DISTRIBUTION_COLUMNS = [
     "natural_deaths:dates",
@@ -34,9 +38,9 @@ DEATH_DISTRIBUTION_COLUMNS = [
 ]
 
 DEATH_DISTRIBUTION_COL_NAMES = {
-    "natural_deaths": _('Natural'),
-    "icu_overflow_deaths": _('Lack of hospital beds'),
-    "hospital_overflow_deaths": _('Lack of ICUs'),
+    "natural_deaths": _("Natural"),
+    "icu_overflow_deaths": _("Lack of hospital beds"),
+    "hospital_overflow_deaths": _("Lack of ICUs"),
 }
 
 PARAMS = [
@@ -52,18 +56,15 @@ PARAMS = [
 ]
 
 CLINICAL = [
-    'hospitalization_period',
-    'icu_period',
-    'hospital_capacity',
-    'icu_capacity',
-    'prob_severe',
-    'prob_critical',
+    "hospitalization_period",
+    "icu_period",
+    "hospital_capacity",
+    "icu_capacity",
+    "prob_severe",
+    "prob_critical",
 ]
 
-CAPACITY = [
-    'hospital_full_capacity',
-    'icu_full_capacity',
-]
+CAPACITY = ["hospital_full_capacity", "icu_full_capacity"]
 
 
 def sidebar(region="BR", disease=covid19, where=st.sidebar):
@@ -73,20 +74,20 @@ def sidebar(region="BR", disease=covid19, where=st.sidebar):
     It receives a region and a disease (defaults to Covid-19) and return a
     dictionary of parameters that might be useful to configure a simulation.
     """
-
-    ui.icon(where=where)
-    region = ui.select_region(region, where=where)
-    params = ui.simulation_params(region, disease, where=where)
+    st = where
+    st.logo()
+    region = st.select_region(region, healthcare_regions=True)
+    params = st.simulation_params(region, disease)
     return {
         "region": region,
         **params,
-        **ui.healthcare_params(region, where=where),
-        **ui.epidemiological_params(region, disease, where=where),
-        **{'runner': ui.select_intervention(params['period'], where=where)},
+        **st.healthcare_params(region),
+        **st.epidemiological_params(region, disease),
+        **{"runner": st.select_intervention(params["period"])},
     }
 
 
-def output(model, info, title=_('Hospital pressure calculator')):
+def output(model, info, title=_("Hospital pressure calculator")):
     """
     Create default output from model.
     """
@@ -95,29 +96,29 @@ def output(model, info, title=_('Hospital pressure calculator')):
 
     summary_cards(model)
 
-    pause(where=st)
-    hospitalizations_chart(info, where=st)
+    st.pause()
+    model.ui.hospitalizations_chart()
 
-    pause(where=st)
-    available_beds_chart(info, where=st)
+    st.pause()
+    model.ui.available_beds_chart()
 
-    line(where=st)
-    population_info_chart(info["age_pyramid"], where=st)
+    st.line()
+    population_info_chart(info["age_pyramid"])
 
-    pause(where=st)
-    deaths_chart(info, where=st)
+    st.pause()
+    deaths_chart(info)
 
-    line(where=st)
-    healthcare_parameters(info, where=st)
+    st.line()
+    healthcare_parameters(info)
 
-    pause(where=st)
-    protection_equipment_demand(info, where=st)
+    st.pause()
+    protection_equipment_demand(info)
 
-    pause(where=st)
-    epidemiological_parameters(info, where=st)
+    st.pause()
+    epidemiological_parameters(info)
 
-    pause(where=st)
-    footnotes(where=st)
+    st.pause()
+    st.footnotes()
 
 
 # @tle_cache('ui.app.calc')
@@ -139,16 +140,20 @@ def model(*, daily_cases, runner, period, disease, **kwargs):
 
 
 def main(region="BR", disease=covid19):
-    ui.css()
+    st.css()
     params = sidebar(region=region, disease=disease)
     debug = False
 
-    if DEBUG and st.checkbox(_('Enable debug')):
-        st.info(_('Running in debug mode!'))
-        ui.html('''
+    if DEBUG and st.checkbox(_("Enable debug")):
+        st.info(_("Running in debug mode!"))
+        st.html(
+            """
         <ul>
             <li><a href="#debug">{}</a></li>
-        </ul>'''.format(_('Debug section!')))
+        </ul>""".format(
+                _("Debug section!")
+            )
+        )
         debug = True
 
     epidemiology = extract_keys(PARAMS, params)
@@ -167,19 +172,19 @@ def main(region="BR", disease=covid19):
     finally:
         if debug:
             if results:
-                ui.html('<div style="height: 15rem"></div>', where=st)
-            ui.html('<h2 id="debug">{}</h2>'.format(_('Debug information')))
+                st.html('<div style="height: 15rem"></div>')
+            st.html('<h2 id="debug">{}</h2>'.format(_("Debug information")))
 
-            st.subheader(_('Generic parameters'))
+            st.subheader(_("Generic parameters"))
             st.write(params)
 
-            st.subheader(_('Epidemiological parameters'))
+            st.subheader(_("Epidemiological parameters"))
             st.write(epidemiology)
 
-            st.subheader(_('Clinical parameters'))
+            st.subheader(_("Clinical parameters"))
             st.write(clinical)
 
-            st.subheader(_('Output'))
+            st.subheader(_("Output"))
             st.write(results)
 
             if m:
@@ -188,7 +193,7 @@ def main(region="BR", disease=covid19):
             if cm:
                 st.line_chart(cm[["infectious", "severe", "critical"]])
 
-                st.subheader(_('Distribution of deaths'))
+                st.subheader(_("Distribution of deaths"))
                 df = cm[DEATH_DISTRIBUTION_COLUMNS]
                 df.columns = [DEATH_DISTRIBUTION_COL_NAMES[k] for k in df.columns]
                 st.area_chart(df)
