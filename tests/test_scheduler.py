d78a2574b302aecd72a61dd3825bef883a0f604a
RCristyan <rcristyan9@gmail.com>
2020-10-29 12:00:08 -0300

    Implemented scheduler pause and stop tests.

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,12 +1,13 @@
-from pydemic_ui.main import *
+from pydemic_ui.scheduler import *
 
 class TestScheduler:
+
     def test_if_a_task_is_scheduled(self):
 
         def say_hello():
             print('hello')
 
-        agendador = scheduler()
+        agendador = Scheduler()
         agendador._clock = datetime_to_time
         agendador._clock_args = datetime.datetime(2020, 9, 15, 12, 30)
         agendador._sleep = lambda: sleep(0)
@@ -15,4 +16,40 @@ class TestScheduler:
         data = datetime.datetime(2020, 9, 15, 12, 40, 3)
         agendador.schedule(say_hello, data)
 
+        agendador.pause()
+
         assert unix_time_to_string(agendador.tasks[0][0]) == '2020-09-15 12:40:03'
+
+    def test_if_scheduler_stops(self):
+        def say_hello():
+            print('hello')
+
+        agendador = Scheduler()
+        agendador._clock = datetime_to_time
+        agendador._clock_args = datetime.datetime(2020, 9, 15, 12, 30)
+        agendador._sleep = lambda: sleep(0)
+        
+        agendador.start()
+        data = datetime.datetime(2020, 9, 15, 12, 40, 3)
+        agendador.schedule(say_hello, data)
+
+        agendador.stop()
+
+        assert len(agendador.tasks) == 0
+
+    def test_if_scheduler_pauses(self):
+        def say_hello():
+            print('hello')
+
+        agendador = Scheduler()
+        agendador._clock = datetime_to_time
+        agendador._clock_args = datetime.datetime(2020, 9, 15, 12, 30)
+        agendador._sleep = lambda: sleep(0)
+        
+        agendador.start()
+        data = datetime.datetime(2020, 9, 15, 12, 40, 3)
+        agendador.schedule(say_hello, data)
+
+        agendador.pause()
+
+        assert len(agendador.tasks) == 1
