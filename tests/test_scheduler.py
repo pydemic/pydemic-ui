1d276fa55fba31cd6c6f83d20fbcefdabff25e12
RCristyan <rcristyan9@gmail.com>
2020-11-03 13:09:29 -0300

    Added monthly schedule test.

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -126,3 +126,45 @@ class TestScheduler:
         agendador.stop()
 
         assert scheduled_dates == expected_dates
+
+    def test_montly_schedule(self):        
+        def say_hello():
+            return 'hello'
+
+        scheduled_dates = []
+        expected_dates = []
+
+        agendador = Scheduler()
+        agendador._clock = datetime_to_time
+        agendador._clock_args = datetime.datetime(2020, 3, 15, 13, 30)
+        agendador._sleep = lambda: sleep(0)
+
+        agendador.start()
+        data = datetime.datetime(2020, 3, 15, 13, 30, 3)
+
+        expected_dates.append('2020-03-15 13:30:03')
+        agendador.schedule_montly(say_hello, data)
+
+        scheduled_dates.append(unix_time_to_string(agendador.tasks[0][0]))
+
+        month_counter = 3
+
+        while month_counter < 10:
+            if unix_time_to_string(agendador.tasks[0][0]) != f'2020-{month_counter}-15 13:30:03':
+
+                month_counter += 1
+
+                agendador._clock_args = datetime.datetime(2020, month_counter, 15, 13, 30, 30)
+                agendador._clock_tick = 0
+                
+                if month_counter > 8:
+                    expected_dates.append(f'2020-{month_counter+1}-15 13:30:03')
+                else:
+                    expected_dates.append(f'2020-0{month_counter+1}-15 13:30:03')
+
+                sleep(0.1)
+                scheduled_dates.append(unix_time_to_string(agendador.tasks[0][0]))
+
+        agendador.stop()
+
+        assert scheduled_dates == expected_dates
